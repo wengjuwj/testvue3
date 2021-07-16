699707d456ab86af24c1ee6038b6fbad987ae4be
wengjuwj <18226137228@163.com>
2021-07-16 16:42:57 +0800

    地图测

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,127 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div id="chinaEcharts" ref="chinaEcharts"   class="leftEcharts" style="height:400px;"></div>
   </div>
 </template>
 
 <script>
+import * as echarts from 'echarts'
+import 'echarts/map/js/china.js'  //如果是npm下载的直接引入包里面的就可以，另外h5可以直接下载下来引入
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      data:[
+          {name:"南海诸岛",value:0, count: 30},
+          {name: '北京', value: 3, count: 30},
+          {name: '天津', value: 5},
+          {name: '上海', value: 6},
+          {name: '重庆', value: 15},
+          {name: '河北', value: 15},
+          {name: '河南', value: 15},
+          {name: '云南', value: 15},
+          {name: '辽宁', value: 7},
+          {name: '黑龙江', value: 5},
+          {name: '湖南', value: 68},
+          {name: '安徽', value: 34},
+          {name: '山东', value: 34},
+          {name: '新疆', value: 34},
+          {name: '江苏', value: 34},
+          {name: '浙江', value: 34},
+          {name: '江西', value: 34},
+          {name: '湖北', value: 34},
+          {name: '广西', value: 34},
+          {name: '甘肃', value: 34},
+          {name: '山西', value: 34},
+          {name: '内蒙古', value: 9},
+          {name: '陕西', value: 16},
+          {name: '吉林', value: 16},
+          {name: '福建', value: 16},
+          {name: '贵州', value: 16},
+          {name: '广东', value: 43},
+          {name: '青海', value: 43},
+          {name: '西藏', value: 43},
+          {name: '四川', value: 43},
+          {name: '宁夏', value: 43},
+          {name: '海南', value: 75},
+          {name: '台湾', value: 75},
+          {name: '香港', value: 75},
+          {name: '澳门', value: 75}
+        ]
+    }
+  },
+  mounted(){
+    this.chinaEcharts(this.data,99)
+  },
+  methods:{
+    //    热力图
+    chinaEcharts(dataList,maxNum) {
+      // let this_ = this;
+      let myChart = echarts.init(document.getElementById('chinaEcharts'));
+      let resizeTimer = null;
+      let option = {
+            tooltip: {
+                    formatter:function(params){
+                        return params.seriesName+'<br />'+params.name+'：'+params.value
+                    }//数据格式化
+                },
+            visualMap: {
+                min: 0,
+                max: maxNum,
+                left: 'left',
+                top: 'bottom',
+                text: ['高','低'],//取值范围的文字
+                inRange: {
+                    color: ['#e0ffff', '#006edd']//取值范围的颜色
+                },
+                show:true//图注
+            },
+            geo: {
+                map: 'china',
+                roam: false,//不开启缩放和平移
+                zoom:1.23,//视角缩放比例
+                label: {
+                    normal: {
+                        show: true,
+                        fontSize:'10',
+                        color: 'rgba(0,0,0,0.7)'
+                    }
+                },
+                itemStyle: {
+                    normal:{
+                        borderColor: 'rgba(0, 0, 0, 0.2)'
+                    },
+                    emphasis:{
+                        areaColor: '#F3B329',//鼠标选择区域颜色
+                        shadowOffsetX: 0,
+                        shadowOffsetY: 0,
+                        shadowBlur: 20,
+                        borderWidth: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            },
+            series : [
+                {
+                    name: '信息量',
+                    type: 'map',
+                    geoIndex: 0,
+                    data:dataList
+                }
+            ]
+        };
+      myChart.setOption(option);
+    
+      window.addEventListener('resize', function () {
+        if (resizeTimer) clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function () {
+          myChart.resize();
+        }, 100)
+      })
+    },
   }
 }
 </script>
